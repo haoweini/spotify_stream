bc24009f8dfc722211e126a2022e7febb1860974
Ni, Haowei [GTSUS] <HNi1@its.jnj.com>
2022-02-27 13:28:10 -0500

    Inital Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a proejct to analyze my spotify metadata.',
+    author='Haowei Ni',
+    license='MIT',
+)
