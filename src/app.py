7399b4ad6c3d7477ec5197313d27c5c8052257a1
Ni, Haowei [GTSUS] <HNi1@its.jnj.com>
2022-02-27 19:52:08 -0500

    Add top artists

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from dis import dis
 import streamlit as st
 from data.get_saved_library import get_saved_library, display_user_name, display_user_pic
 from data.get_recently_played import get_recently_played
+from data.get_top_artists import get_top_artists
 from data.image_url import path_to_image_html
 from PIL import Image
 import requests
@@ -12,7 +13,7 @@ import streamlit.components.v1 as components
 # Title at the top of the application 
 st.title('Spotify Stream')
 
-# Get Songs from Saved Library
+# Get Songs from Recently Played
 st.text('You just listened these 5 songs')
 df_recent = get_recently_played()
 df_recent = df_recent.head(5)
@@ -20,6 +21,16 @@ df_recent.to_html('../data/interim/recently_webpage.html',escape=False, formatte
 HtmlFile = open("../data/interim/recently_webpage.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 components.html(source_code, height =400, width = 1000)
+
+# Get Artists from Top Artists
+st.text('Your Top 5 Artists')
+df_top_artists = get_top_artists()
+df_top_artists = df_top_artists.head(5)
+df_top_artists.to_html('../data/interim/top_artists_webpage.html',escape=False, formatters=dict(artist=path_to_image_html))
+HtmlFile = open("../data/interim/top_artists_webpage.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+components.html(source_code, height =400, width = 1000)
+
 
 # Get Songs from Saved Library
 df_saved = get_saved_library()
