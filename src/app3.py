83fd13d4da9fb54a595611d4c0cd594eb5b8a9fd
Ni, Haowei [GTSUS] <HNi1@its.jnj.com>
2022-03-07 16:32:29 -0500

    add heroku

--- src/app3.py
+++ src/app3.py
@@ -5,25 +5,23 @@ import json
 import spotipy
 import streamlit as st
 
+username = st.text_input('Movie title', '')
+
 with open('../data/raw/spotify_creds.json') as f:
     spotify_creds = json.load(f)
 
 client_id = spotify_creds['client_id']
 client_secret = spotify_creds['client_secret']
-username = spotify_creds['username']
+#username = spotify_creds['username']
 scope = spotify_creds['saved_library_scope']
 redirect_uri = spotify_creds['saved_library_redirect_url']
 
-def get_token(scope):
-    
-    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret) 
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
-    
-    return token
 
-all_token = get_token('user-library-read user-read-recently-played user-top-read')
-
-st.text(all_token)
+    
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret) 
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+    
+st.text(token)
 
 
