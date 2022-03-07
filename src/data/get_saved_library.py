aceae57c2903c39ff85817aa8bd687485d2f3689
Ni, Haowei [GTSUS] <HNi1@its.jnj.com>
2022-03-07 16:00:53 -0500

    add genre treemap

--- src/data/get_saved_library.py
+++ src/data/get_saved_library.py
@@ -62,6 +62,7 @@ def get_saved_library():
     track_list = ''
     added_ts_list = []
     artist_list = []
+    artist_url_list = []
     title_list = []
     popularity_list = []
     album_cover_list = []
@@ -70,7 +71,6 @@ def get_saved_library():
     offset_index = 0
 
     while more_songs:
-        sp = connect_to_spotify_api(client_id, client_secret, username, scope, redirect_uri)
         songs = sp.current_user_saved_tracks(offset=offset_index)
         for song in songs['items']:
             #join track ids to a string for audio_features function
@@ -92,6 +92,14 @@ def get_saved_library():
             for artist in artists:
                 artists_name += artist['name']  + ','
             artist_list.append(artists_name[:-1])
+            # aritst_url
+            artists = song['track']['artists']
+            artists_urls = ''
+            for artist in artists:
+                artist_url = artist['external_urls']['spotify']
+                artist_url = artist_url.split('/')[-1]
+                artists_urls += artist_url  + ','
+            artist_url_list.append(artists_urls[:-1])
             
         track_features = sp.audio_features(track_list[:-1])
         df_temp = pd.DataFrame(track_features)
@@ -108,6 +116,7 @@ def get_saved_library():
     #include timestamp added, title and artists of a song
     df_saved_tracks['song_title'] = title_list
     df_saved_tracks['artists'] = artist_list
+    df_saved_tracks['artists_url'] = artist_url_list
     df_saved_tracks['date_added'] = added_ts_list
     df_saved_tracks['album_cover'] = album_cover_list
     df_saved_tracks['popularity'] = popularity_list
@@ -117,3 +126,44 @@ def get_saved_library():
     df_saved_tracks['date_added_year'] = df_saved_tracks['date_added'].dt.year
     
     return df_saved_tracks
+
+def get_all_genre(df):
+    sp = connect_to_spotify_api(client_id, client_secret, username, scope, redirect_uri)
+    artist_list = []
+
+    for i in range(len(df)):
+        artists = df.iloc[i]['artists_url']
+        for artist in artists.split(','):
+            artist_list.append(artist)
+
+    artist_list = set(artist_list)
+    
+    df_artist_genre = pd.DataFrame()
+    aritst_name_list = []
+    aritst_genre_list = []
+
+    for i in artist_list:
+        aritst_name = sp.artist(i)['name']
+        #print(aritst_name)
+        aritst_genre = sp.artist(i)['genres']
+        aritst_name_list.append(aritst_name)
+        aritst_genre_list.append(aritst_genre)
+
+    df_artist_genre['name'] = aritst_name_list
+    df_artist_genre['genre'] = aritst_genre_list
+    
+    df_artist_detailed_genre = pd.DataFrame()
+    aritst_name_list = []
+    aritst_genre_list = []
+
+    for i in range(len(df_artist_genre)):
+        artist = df_artist_genre.iloc[i]['name']
+        genre_list = df_artist_genre.iloc[i]['genre']
+        for genre in genre_list:
+            aritst_name_list.append(artist)
+            aritst_genre_list.append(genre)
+
+    df_artist_detailed_genre['name'] = aritst_name_list
+    df_artist_detailed_genre['genre'] = aritst_genre_list
+    
+    return df_artist_detailed_genre
--- src/pages/welcome.py
+++ src/pages/welcome.py
@@ -1,32 +1,64 @@
+from matplotlib import container
 import streamlit as st
 import numpy as np
 import pandas as pd
 from dis import dis
 import streamlit as st
-from data.get_saved_library import display_user_name, display_user_pic
+from data.get_saved_library import display_user_name, get_all_genre, get_saved_library
 from data.get_top_artists import get_top_tracks
 from PIL import Image
-import requests
-from io import BytesIO
 from IPython.core.display import HTML
 import streamlit.components.v1 as components
+import plotly.express as px
 
 # @st.cache
 def app():
-    col1, col2 = st.columns(2)
-    with col1:
-        username = display_user_name()
-        #pic_url = display_user_pic()
-        user_pic = Image.open('../data/raw/user_pic.jpg')
-        st.text("Welcome %s !" % (username))
-        st.image(user_pic, width=500, use_column_width='always')
-    
-    with col2:
-        st.text('These Are Your Top 5 Tracks')
-        df_top_tracks = get_top_tracks()
-        df_top_tracks = df_top_tracks.head(5)
-        track_urls = list(df_top_tracks['url'])
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            username = display_user_name()
+            #pic_url = display_user_pic()
+            user_pic = Image.open('../data/raw/user_pic.jpg')
+            st.text("Welcome %s !" % (username))
+            st.image(user_pic, width=500, use_column_width='always')
+        
+        with col2:
+            st.text('These Are Your Top 5 Tracks')
+            df_top_tracks = get_top_tracks()
+            df_top_tracks = df_top_tracks.head(5)
+            track_urls = list(df_top_tracks['url'])
 
-        for uri in track_urls:
-            track = """<iframe src="https://open.spotify.com/embed/track/{}" width="460" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>""".format(uri)
-            components.html(track,height=100, )
+            for uri in track_urls:
+                track = """<iframe src="https://open.spotify.com/embed/track/{}" width="460" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>""".format(uri)
+                components.html(track,height=100, )
+
+    with st.container():
+        df_saved = get_saved_library()
+        # get all artists in saved library
+        df_artist_detailed_genre = get_all_genre(df_saved)
+
+        genre_new = []
+        for i in range(len(df_artist_detailed_genre)):
+            genre = df_artist_detailed_genre.iloc[i]['genre']
+            if 'pop' in genre:
+                genre_new.append('pop')
+            elif 'edm' in genre:
+                genre_new.append('edm')
+            elif 'house' in genre:
+                genre_new.append('house')
+            elif 'big room' in genre:
+                genre_new.append('big room')
+            elif 'trance' in genre:
+                genre_new.append('trance')
+            elif 'elect' in genre:
+                genre_new.append('edm')
+            elif 'rap' in genre:
+                genre_new.append('rap')
+            elif 'dance' in genre:
+                genre_new.append('dance')
+            else:
+                genre_new.append('other')
+                
+        df_artist_detailed_genre['genre_new'] = genre_new
+        fig = px.treemap(df_artist_detailed_genre, path=['genre_new'])
+        st.plotly_chart(fig, width=1500, use_container_width=True)
